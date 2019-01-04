8f9ae48bc747afd3967bfe9e59901eed30307160
Kyle Abarquez <kabarquez8@gmail.com>
2019-01-03 16:39:14 -0800

    2nd

--- src/App.js
+++ src/App.js
@@ -2,24 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Aggregate extends Component {
+    render () {
+        return (
+            <div style={{width: '40%', display: 'inline-block'}}>
+                <h2 style={{color: '#ddd'}}>Number Text</h2>
+            </div>
+        );
+    }
+}
+
+class Filter extends Component {
+    render() {
+        return(
+            <div>
+                <img/>
+                <input type="text"/>
+            </div>
+        );
+    }
+}
+
+class Playlist extends Component {
+    render() {
+        return (
+            <div style={{width: '25%', display: 'inline-block'}}>
+                <img/>
+                <h3>Playlist Name</h3>
+                <ul style={{listStyle: 'none', margin: '0', padding: '0'}}>
+                    <li>Song 1</li>
+                    <li>Song 2</li>
+                    <li>Song 3</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
 class App extends Component {
   render() {
+    let name ="Kyle"
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
   }
